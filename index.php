ea313b24cbb33938877733afdac81b57ccc2de60
canon9375 <36266115+canon9375@users.noreply.github.com>
2019-04-11 10:59:08 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,203 +1,197 @@
 <!doctype html>
-<?php
-//	// This is the data you want to pass to Python
-//$data = array('as', 'df', 'gh');
-//
-//// Execute the python script with the JSON data
-//$result = shell_exec('python ../loadRandom.py ' . escapeshellarg(json_encode($data)));
-//$result = shell_exec('python ../loadRandom.py');
-//$resultData = json_decode($result, true);
-//var_dump($resultData);
-//$cmd = escapeshellcmd('python /Applications/XAMPP/xamppfiles/htdocs/fyp/demo/loadRandom.py /tmp 2>&1'); 
-//$output= exec('/usr/bin/python /Applications/XAMPP/xamppfiles/htdocs/fyp/demo/loadRandom.py /tmp 2>&1');
-//echo $output;
-?>
-<html>
+<html lang="en">
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type="text/javascript" src="jslib/jquery-1.11.1.js"></script>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no, width=device-width">
-    <title>air quality forecast</title>
-    <link rel="stylesheet" href="https://a.amap.com/jsapi_demos/static/demo-center/css/demo-center.css" />
-   <script type="text/javascript" src="https://webapi.amap.com/maps?v=1.4.11&key=您申请的key值"></script>
-<script src="https://a.amap.com/jsapi_demos/static/demo-center/js/demoutils.js"></script>
     <style>
-        html,
-        body,
-        #container {
-          width: 80%;
-          height: 80%;
-            margin-left: 10%;
-        }
-
-        h1{
-            margin: 50px;
-            font-size: 40px;
+        body{
+            background-image: url("photo/bgc.jpg");
         }
 
 
-
-        .button {
-
-
-            color: black;
-            padding: 10px 22px;
-            text-align: center;
-            text-decoration: none;
-            display: inline-block;
-            font-size: 16px;
-            border: 2px solid #e7e7e7;
-            background-color: white;
-            transition-duration: 1s;
+        .accordion {
+            background-color: #eee;
+            color: #444;
             cursor: pointer;
-        margin-bottom: 30px;
+            padding: 18px;
+            width: 100%;
+            height: 70px;
+            border: none;
+            text-align: left;
+            outline: none;
+            font-size: 15px;
+        }
+
+        .active, .accordion:hover {
+            background-color: #ccc;
+            color: #18ff54;
+        }
+
+        .panel {
+
+            background-color: white;
+            overflow: hidden;
+            width: 100%;
+        }
+
+        #right{
+           position: absolute;
+            left: 10%;
+            width: 20%;
+            height: 100px;
+            display: inline-block;
+        }
+        #left{
+            position: absolute;
+            border: 1px solid red;
+            right: 10%;
+            width: 60%;
+            height: 700px;
+            display: inline-block;
         }
 
 
-        .button:hover {background-color: #e7e7e7;}
-        table {
-            border-collapse: collapse;
-            width: 80%;
-            margin-bottom: 30px;
+        .accordion{
+
             font-size: 25px;
         }
-
-        th, td {
-            padding: 8px;
-            text-align: left;
-            border-bottom: 1px solid #ddd;
+        .accordion b{
+            color: #18ff54;
+            right: 0px;
+            margin-right: 0px;
+            font-size: 30px;
         }
 
-        #value{
-            width: 50%;
-            border-collapse: collapse;
-            font-weight: bold;
+        .panel p:hover{
+            color: red;
+            background-color: #ccc;
+        }
+        .panel p{
+            width: 99%;
+            margin: 0px;
+            padding-top: 3%;
+            height: 40px;
             font-size: 20px;
-            text-align: center;
 
         }
+        h2{
+           color: red;
+            font-size: 70px;
+            width: 80%;
+            margin-bottom: 0px;
+            margin-top: 0px;
+            padding-top: 10px;
+            padding-bottom: 10px;
 
+        }
+        h2 img{
 
-        #value , th, td {
-            padding: 20px;
-            border: 1px solid #ddd;
+            width: 100px;
+            height: 100px;
         }
     </style>
+
     <script>
 
         $(function(){
-            $("#info").hide();
-            $("#more").click(function(){
-                $("#info").toggle(0);
+            $(".panel").hide();
+            // $("#left").load("./MainPage/mapDisplay.php");
+            $("#firstMenu").click(function () {
+                $("#firstMenuBar").toggle();
             });
+            $("#secondMenu").click(function () {
+                $("#secondMenuBar").toggle();
+            });
+            $("#thirdMenu").click(function () {
+                $("#thirdMenuBar").toggle();
+            });
+            $("#FouthMenu").click(function () {
+                $("#FourthMenuBar").toggle();
+            });
+            $("#fiveMenu").click(function () {
+                $("#fifthMenuBar").toggle();
+            });
+            $("#SixMenu").click(function () {
+                $("#SixMenuBar").toggle();
+            });
+            $("#APImenu").click(function () {
+                $("#APIMenuBar").toggle();
+            });
+
+
         });
 
     </script>
 </head>
 <body>
 
-<h1 style="text-align: center"> <i> Air Quality Forecast</i></h1>
 <center>
+    <h2><i>Air Quality Forcast</i> </h2>
 
-    <button class="button">Web Scraping</button>
-    <button class="button">Train Predictor</button>
-    <button class="button" id="more">AQHI Information</button>
+<div id="right">
+    <button class="accordion" id="firstMenu" ">Main Page<b>&#43;</b></button>
+    <div class="panel" id="firstMenuBar">
+        <p>Multiple Linear Regression</p>
+        <p>Random Forest</p>
+        <p>Time Series(ARIMA)</p>
+        <p>ANN</p>
+        <p>mathematic</p>
+    </div>
+    <button class="accordion" id="APImenu">Get Data<b>&#43;</b></button>
+    <div class="panel" id="APIMenuBar">
+      <p>  <a href="./callAPI/ARIMA.html" target="con"> Copy Link</a></p>
 
-</center>
-<center>
-<div id="info">
-    <table id="value">
+    </div>
+    <button class="accordion" id="secondMenu">Chart<b>&#43;</b></button>
+    <div class="panel" id="secondMenuBar">
+        <p><a href="./chart/chart.php" target="con">Last Compare</a></p>
+        <p>Predict Data</p>
+    </div>
 
-        <tr>
-            <th>Level</th>
-            <th>Value</th>
+    <button class="accordion" id="thirdMenu">Test Plan<b>&#43;</b> </button>
+    <div class="panel" id="thirdMenuBar">
+        <p><a href="./TestPlan/MLP.html" target="con"> Multiple Linear Regression</a> </p>
+        <p><a href="./TestPlan/rf.html" target="con">Random Forest</a></bu></p>
+        <p><a href="./TestPlan/ARIMAMain.html" target="con">Time Series(ARIMA)</a></p>
+        <p></p>
+<!--        <p>ANN</p>-->
+    </div>
 
-        </tr>
-        <tr style="color: green">
-            <td>Low</td>
-            <td>1-4</td>
+    <button class="accordion" id="FouthMenu">Explaination<b>&#43;</b></button>
+    <div class="panel" id="FourthMenuBar">
+        <p>Multiple Linear Regression</p>
+        <p>Random Forest</p>
+        <p><a href="./Explaination/ARIMA.html" target="con">Time Series(ARIMA)</a></p>
 
-        </tr>
-        <tr style="color: red">
-            <td>moderate</td>
-            <td>5-8 </td>
-        </tr>
+        <p>ANN</p>
+        <p><a href="./Explaination/math.html" target="con"> mathematic</a> </p>
 
-        <tr style="color: black">
-            <td>Series</td>
-            <td>9-10+</td>
-        </tr>
-    </table>
+    </div>
+    <button class="accordion" id="fiveMenu">Past Data<b>&#43;</b></button>
+    <div class="panel" id="fifthMenuBar">
+        <p>Weather Data</p>
+        <p>air pollution</p>
+    </div>
+    <button class="accordion" id="SixMenu">Get AQHI<b>&#43;</b></button>
+    <div class="panel" id="SixMenuBar">
+        <p>By GPS</p>
+        <p>By Location</p>
+    </div>
+
+</div>
+<div id="left">
+
+    <iframe name="con" src="./MainPage/mapDisplay.php" frameborder="0" width="100%" height="100%">
+
+    </iframe>
 
 </div>
 </center>
-<?php
-date_default_timezone_set("Asia/Hong_Kong");
-$first=date('H')+1;
-$se=date('H')+2;
-$th=date('H')+3;
-?>
-<center>
-    <table>
-        <tr>
-            <th> </th>
-            <th> <?php echo $first. ":00" ;?></th>
-            <th> <?php echo $se. ":00";?></th>
-            <th> <?php echo $th. ":00";?></th>
-
-        </tr>
-
-        <tr>
-            <td>Multiple Linear Regression </td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-
-
-        </tr>
-        <tr>
-            <td>Random Forest</td>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-
-        </tr>
-    </table>
-
-</center>
-    <div id="container"></div>
-    <span class="info" id="text">
-
-</span>
-
-    <script type="text/javascript">
-        //初始化地图对象，加载地图
-        var map = new AMap.Map("container", {
-            resizeEnable: true,
-            zoom: 11,
-            showIndoorMap: false
-        });
-        var bounds =
-            map.getBounds();
-        map.setLimitBounds(bounds);
-        map.on('moveend', showInfoClick);
-        function showInfoClick(e){
-//        var text = '您在 [ '+e.lnglat.getLng()+','+e.lnglat.getLat()+' ] 的位置单击了地图！'
-//        document.querySelector("#text").innerText = text;
-            map.getCity( function(info){
-                var node = new PrettyJSON.view.Node({
-                    el: document.querySelector("#text"),
-                    data: info
-                });
-            });
-
-        }
-
-
-
-    </script>
-
 
 </body>
 </html>
