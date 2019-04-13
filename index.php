354821d952c11d19ad52317e921d0a0f94cd118e
canon9375 <36266115+canon9375@users.noreply.github.com>
2019-04-13 16:08:27 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,197 +1,135 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script type="text/javascript" src="jslib/jquery-1.11.1.js"></script>
-    <style>
-        body{
-            background-image: url("photo/bgc.jpg");
-        }
+<!DOCTYPE html>
+<html>
+<title></title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style>
+.w3-sidebar a {font-family: "Roboto", sans-serif}
+body,h1,h2,h3,h4,h5,h6,.w3-wide {font-family: "Montserrat", sans-serif;}
+</style>
+<body class="w3-content" style="max-width:1200px">
 
-
-        .accordion {
-            background-color: #eee;
-            color: #444;
-            cursor: pointer;
-            padding: 18px;
-            width: 100%;
-            height: 70px;
-            border: none;
-            text-align: left;
-            outline: none;
-            font-size: 15px;
-        }
-
-        .active, .accordion:hover {
-            background-color: #ccc;
-            color: #18ff54;
-        }
-
-        .panel {
-
-            background-color: white;
-            overflow: hidden;
-            width: 100%;
-        }
-
-        #right{
-           position: absolute;
-            left: 10%;
-            width: 20%;
-            height: 100px;
-            display: inline-block;
-        }
-        #left{
-            position: absolute;
-            border: 1px solid red;
-            right: 10%;
-            width: 60%;
-            height: 700px;
-            display: inline-block;
-        }
-
-
-        .accordion{
-
-            font-size: 25px;
-        }
-        .accordion b{
-            color: #18ff54;
-            right: 0px;
-            margin-right: 0px;
-            font-size: 30px;
-        }
-
-        .panel p:hover{
-            color: red;
-            background-color: #ccc;
-        }
-        .panel p{
-            width: 99%;
-            margin: 0px;
-            padding-top: 3%;
-            height: 40px;
-            font-size: 20px;
-
-        }
-        h2{
-           color: red;
-            font-size: 70px;
-            width: 80%;
-            margin-bottom: 0px;
-            margin-top: 0px;
-            padding-top: 10px;
-            padding-bottom: 10px;
-
-        }
-        h2 img{
-
-            width: 100px;
-            height: 100px;
-        }
-    </style>
-
-    <script>
-
-        $(function(){
-            $(".panel").hide();
-            // $("#left").load("./MainPage/mapDisplay.php");
-            $("#firstMenu").click(function () {
-                $("#firstMenuBar").toggle();
-            });
-            $("#secondMenu").click(function () {
-                $("#secondMenuBar").toggle();
-            });
-            $("#thirdMenu").click(function () {
-                $("#thirdMenuBar").toggle();
-            });
-            $("#FouthMenu").click(function () {
-                $("#FourthMenuBar").toggle();
-            });
-            $("#fiveMenu").click(function () {
-                $("#fifthMenuBar").toggle();
-            });
-            $("#SixMenu").click(function () {
-                $("#SixMenuBar").toggle();
-            });
-            $("#APImenu").click(function () {
-                $("#APIMenuBar").toggle();
-            });
-
-
-        });
-
-    </script>
-</head>
-<body>
-
-<center>
-    <h2><i>Air Quality Forcast</i> </h2>
-
-<div id="right">
-    <button class="accordion" id="firstMenu" ">Main Page<b>&#43;</b></button>
-    <div class="panel" id="firstMenuBar">
-        <p>Multiple Linear Regression</p>
-        <p>Random Forest</p>
-        <p>Time Series(ARIMA)</p>
-        <p>ANN</p>
-        <p>mathematic</p>
+<!-- Sidebar/menu -->
+<nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+  <div class="w3-container w3-display-container w3-padding-16">
+    <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+    <h3 class="w3-wide"><b>Air Quality Prediction</b></h3>
+  </div>
+  <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+	  <a onclick="myAccFunc()" href="javascript:void(0)" class="w3-button w3-block w3-white w3-left-align" id="myBtn">
+      Main <i class="fa fa-caret-down"></i>
+    </a>
+    <div id="demoAcc" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+      
+      <a href="./MainPage/mapDisplay.php" class="w3-bar-item w3-button" target="con">Random Forest</a>
+      <a href="#" class="w3-bar-item w3-button" target="con">Time Series(ARIMA)</a>
+      <a href="#" class="w3-bar-item w3-button"target="con">ANN</a>
+      <a href="#" class="w3-bar-item w3-button"target="con">Mathematic</a>
     </div>
-    <button class="accordion" id="APImenu">Get Data<b>&#43;</b></button>
-    <div class="panel" id="APIMenuBar">
-      <p>  <a href="./callAPI/ARIMA.html" target="con"> Copy Link</a></p>
-
+    <a href="./callAPI/ARIMA.html" class="w3-bar-item w3-button" target="con">Get Data</a>
+    
+    <a href="./chart/chart.php" target="con" class="w3-bar-item w3-button">Chart</a>
+    <a href="#" class="w3-bar-item w3-button" onClick="show3()">Test Plan<i class="fa fa-caret-down"></i></a>
+	  <div id="demoAcc3" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+      <a href="./TestPlan/MLP.html" class="w3-bar-item w3-button" target="con">Random Forest</a>
+      <a href="./TestPlan/ARIMAMain.html" class="w3-bar-item w3-button" target="con">Time Series(ARIMA)</a>
+      <a href="./TestPlan/MLP.html" class="w3-bar-item w3-button" target="con">Multiple Linear Regression</a>
     </div>
-    <button class="accordion" id="secondMenu">Chart<b>&#43;</b></button>
-    <div class="panel" id="secondMenuBar">
-        <p><a href="./chart/chart.php" target="con">Last Compare</a></p>
-        <p>Predict Data</p>
+	  
+    <a href="#" class="w3-bar-item w3-button" onClick="show2()" >Explaination<i class="fa fa-caret-down"></i></a>
+	<div id="demoAcc2" class="w3-bar-block w3-hide w3-padding-large w3-medium">
+      <a href="./Explaination/rf.html" class="w3-bar-item w3-button" target="con">Random Forest</a>
+      <a href="./Explaination/ARIMA.html" class="w3-bar-item w3-button" target="con">Time Series(ARIMA)</a>
+      <a href="./Explaination/math.html" class="w3-bar-item w3-button" target="con">Mathematic</a>
     </div>
+    <a href="#" class="w3-bar-item w3-button">Past Data</a>
+    <a href="#" class="w3-bar-item w3-button">Get AQHI</a>
+  </div>
 
-    <button class="accordion" id="thirdMenu">Test Plan<b>&#43;</b> </button>
-    <div class="panel" id="thirdMenuBar">
-        <p><a href="./TestPlan/MLP.html" target="con"> Multiple Linear Regression</a> </p>
-        <p><a href="./TestPlan/rf.html" target="con">Random Forest</a></bu></p>
-        <p><a href="./TestPlan/ARIMAMain.html" target="con">Time Series(ARIMA)</a></p>
-        <p></p>
-<!--        <p>ANN</p>-->
+</nav>
+<iframe  style="display: inline-block"width="100%" height="100%"  name="con" src="./MainPage/mapDisplay.php" frameborder="0">
+	
+	</iframe>
+	
+	
+
+<!-- Top menu on small screens -->
+<header class="w3-bar w3-top w3-hide-large w3-black w3-xlarge">
+  <div class="w3-bar-item w3-padding-24 w3-wide">Air Quality Predict</div>
+  <a href="javascript:void(0)" class="w3-bar-item w3-button w3-padding-24 w3-right" onclick="w3_open()"><i class="fa fa-bars"></i></a>
+</header>
+
+<!-- Overlay effect when opening sidebar on small screens -->
+<div class="w3-overlay w3-hide-large" onclick="w3_close()" style="cursor:pointer" title="close side menu" id="myOverlay"></div>
+
+<!-- !PAGE CONTENT! -->
+<div class="w3-main" style="margin-left:250px">
+
+  <!-- Push down content on small screens -->
+  <div class="w3-hide-large" style="margin-top:83px"></div>
+  
+  <!-- Top header -->
+  <header class="w3-container w3-xlarge">
+
+
+<!-- Newsletter Modal -->
+<div id="newsletter" class="w3-modal">
+  <div class="w3-modal-content w3-animate-zoom" style="padding:32px">
+    <div class="w3-container w3-white w3-center">
+      <i onclick="document.getElementById('newsletter').style.display='none'" class="fa fa-remove w3-right w3-button w3-transparent w3-xxlarge"></i>
+      <h2 class="w3-wide">NEWSLETTER</h2>
+      <p>Join our mailing list to receive updates on new arrivals and special offers.</p>
+      <p><input class="w3-input w3-border" type="text" placeholder="Enter e-mail"></p>
+      <button type="button" class="w3-button w3-padding-large w3-red w3-margin-bottom" onclick="document.getElementById('newsletter').style.display='none'">Subscribe</button>
     </div>
-
-    <button class="accordion" id="FouthMenu">Explaination<b>&#43;</b></button>
-    <div class="panel" id="FourthMenuBar">
-        <p>Multiple Linear Regression</p>
-        <p>Random Forest</p>
-        <p><a href="./Explaination/ARIMA.html" target="con">Time Series(ARIMA)</a></p>
-
-        <p>ANN</p>
-        <p><a href="./Explaination/math.html" target="con"> mathematic</a> </p>
-
-    </div>
-    <button class="accordion" id="fiveMenu">Past Data<b>&#43;</b></button>
-    <div class="panel" id="fifthMenuBar">
-        <p>Weather Data</p>
-        <p>air pollution</p>
-    </div>
-    <button class="accordion" id="SixMenu">Get AQHI<b>&#43;</b></button>
-    <div class="panel" id="SixMenuBar">
-        <p>By GPS</p>
-        <p>By Location</p>
-    </div>
-
+  </div>
 </div>
-<div id="left">
 
-    <iframe name="con" src="./MainPage/mapDisplay.php" frameborder="0" width="100%" height="100%">
+<script>
+// Accordion 
+function myAccFunc() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function show2() {
+  var x = document.getElementById("demoAcc2");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+function show3() {
+  var x = document.getElementById("demoAcc3");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+// Click on the "Jeans" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn").click();
 
-    </iframe>
 
-</div>
-</center>
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+</script>
 
 </body>
 </html>
